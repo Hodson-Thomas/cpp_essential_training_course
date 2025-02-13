2b028fc405b83caef6256b6472428418af3fcd5f
Thomas HODSON <thomas.hodson.pro@gmail.com>
2025-02-13 10:45:53 +0100

    challenge 5

---
+++ challenge5.cpp
@@ -0,0 +1,27 @@
+//
+// Created by thoma on 13/02/2025.
+//
+
+#include <format>
+#include <iostream>
+
+using std::format;
+using std::cout;
+
+template<typename T>
+T factorial(T n) {
+    T result = n;
+    while (n > 1) {
+        result *= --n;
+    }
+    return result;
+}
+
+// int main() {
+//     unsigned long n {5};
+//     unsigned int m {5};
+//
+//     cout << format("Factorial of long {} = {}\n", n, factorial(n));
+//     cout << format("Factorial of int  {} = {}\n", m, factorial(m));
+//     return 0;
+// }
